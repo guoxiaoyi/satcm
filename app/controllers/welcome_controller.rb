71aa21888e4466ad8db422ae61fa46f75936d48d
guoxiaoyi <guoxiaoyi888@gmail.com>
2021-10-19 14:14:45 +0800

    新闻排序

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @news = ArticleCategory.find_by(custom_key: '新闻')
-    @articles = Article.where(article_category_id: @news.descendants.pluck(:id)).limit(3).order(created_at: :asc)
+    @articles = Article.where(article_category_id: @news.descendants.pluck(:id)).limit(3).order(created_at: :desc)
     @office_build = ArticleCategory.find_by(custom_key: '科室建设')
 
     # p Dir.entries(Rails.public_path)
